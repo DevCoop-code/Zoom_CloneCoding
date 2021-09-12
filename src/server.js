7a105963c866296e801673775a059626e89490fd
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-09-12 10:22:45 +0900

    feat: Add & Run WebSocket Server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
 import express from "express";
+
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,7 +9,14 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://:3000');
 
-app.listen(3000); 
+// app.listen(3000); 
+const server = http.createServer(app);
+
+// Running HTTP & WebSocket Server Both
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);
