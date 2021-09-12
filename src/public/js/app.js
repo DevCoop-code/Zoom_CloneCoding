f0a743e5255cd18ae79f4418c671c01e244e2151
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-09-12 15:30:26 +0900

    feat: using socket.emitt & socket listener

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = io();    // Automatically Make & Connect the socket
+
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    // 1st parameter: Event Name
+    // 2nd parameter: Object which is sending Server
+    // 3rd parameter: Callback Func which is sending Server
+    socket.emit("enter_room", {payload: input.value}, () => {
+        console.log("server is done");
+    });
+    input.value="";
+}
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -20,7 +20,12 @@ const server = http.createServer(app);
 
 const io = SocketIO(server);
 io.on("connection", socket => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done()
+        }, 10000);
+    });
 });
 /*
 *** WebSocket Code ***
